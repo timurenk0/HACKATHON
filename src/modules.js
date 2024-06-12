9a2b37d65b0adad88613a8de8860c30cd4af16df
timurenk0 <160054940+timurenk0@users.noreply.github.com>
2024-06-12 02:59:10 +0200

    Add files via upload

---
+++ src/modules.js
@@ -0,0 +1,4 @@
+const calendarButton = document.getElementById('calendar-button')
+const performanceButton = document.getElementById('performance-button')
+const listButton = document.getElementById('list-button')
+const profileButton = document.getElementById('profile-button')
